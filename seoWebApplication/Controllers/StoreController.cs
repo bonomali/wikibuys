54cf17e8f9f5b9d66d1a1e87759965590e74ed52
Ty Moodley <ty.moodley@gmail.com>
2014-11-27 11:09:01 -0500

    Added stores to site

---
+++ seoWebApplication/Controllers/StoreController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using seoWebApplication.Service;
+using seoWebApplication.Models;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+
+namespace seoWebApplication.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class StoreController : Controller
+    {
+        private StoreService _StoreService = new StoreService();
+
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Store/Details/5
+        public ActionResult Details(Guid id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            seoWebApplication.Models.mStores store = _StoreService.Getstore(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
+
+        // GET: /Store/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult EditingPopup_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(_StoreService.Getstores().ToDataSourceResult(request));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult EditingPopup_Create([DataSourceRequest] DataSourceRequest request, mStores store)
+        {
+            if (store != null && ModelState.IsValid)
+            {
+                _StoreService.Create(store);
+            }
+
+            return Json(new[] { store }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult EditingPopup_Update([DataSourceRequest] DataSourceRequest request, mStores store)
+        {
+            if (store != null && ModelState.IsValid)
+            {
+                _StoreService.Update(store);
+            }
+
+            return Json(new[] { store }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult EditingPopup_Destroy([DataSourceRequest] DataSourceRequest request, mStores store)
+        {
+            if (store != null)
+            {
+                _StoreService.Delete(store.Id);
+            }
+
+            return Json(new[] { store }.ToDataSourceResult(request, ModelState));
+        }
+
+        public ActionResult Stores_Read([DataSourceRequest]DataSourceRequest request)
+        { 
+            var stores = (from e in _StoreService.Getstores()
+                            select e).ToList();
+
+            DataSourceResult result = stores.ToDataSourceResult(request);
+            return Json(result);
+        }  
+
+        // POST: /Store/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost, ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(mStores Store)
+        {
+            if (ModelState.IsValid)
+            {
+                _StoreService.Create(Store);
+                return RedirectToAction("Index");
+            }
+
+            return View(Store);
+        }
+
+        // GET: /Store/Edit/5
+        public ActionResult Edit(Guid id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            seoWebApplication.Models.mStores store = _StoreService.Getstore(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
+
+        // POST: /Store/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost, ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(mStores Store)
+        {
+            if (ModelState.IsValid)
+            {
+                _StoreService.Update(Store);
+                return RedirectToAction("Index");
+            }
+            return View(Store);
+        }
+
+        // GET: /Store/Delete/5
+        public ActionResult Delete(Guid id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            mStores store = _StoreService.Getstore(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
+
+        // POST: /Store/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            _StoreService.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ seoWebApplication/Service/StoreService.cs
@@ -0,0 +1,167 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
+using seoWebApplication.DAL;
+using seoWebApplication.Models; 
+using System;
+using System.Collections.Generic;
+using System.Linq; 
+
+namespace seoWebApplication.Service
+{
+    public class StoreService
+    {
+
+        private readonly MongoHelper<mStores> _store;
+        public StoreService()
+        {
+            _store = new MongoHelper<mStores>();
+        }
+
+        public void Create(mStores store)
+        { 
+            store.InsertDate = DateTime.Now;
+            store.InsertENTUserAccountId = 1;
+            _store.Collection.Insert(store);
+        }
+
+        public IEnumerable<mStores> Getstores(int limit, int skip)
+        {
+            var storesCursor = _store.Collection.FindAllAs<mStores>()
+                .SetSortOrder(SortBy<mStores>.Ascending(p => p.name))
+                .SetLimit(limit)
+                .SetSkip(skip)
+                .SetFields(Fields<mStores>.Include(p => p.Id, p => p.name));
+            return storesCursor;
+        }
+ 
+
+
+        public IList<mStores> Getstores()
+        {
+            try
+            { 
+                return _store.Collection.FindAll().ToList<mStores>();
+            }
+            catch (MongoConnectionException)
+            {
+                return new List<mStores>();
+            }
+        }
+
+        public IList<mStores> GetstoresByDepartment(int Id)
+        {
+            try
+            {
+                return _store.Collection.Find(Query.EQ("department_id", Id)).ToList<mStores>();
+
+            }
+            catch (MongoConnectionException)
+            {
+                return new List<mStores>();//
+            }
+        }
+
+
+ 
+
+        public IList<mStores> GetstoresByCategory(int Id)
+        {
+            try
+            { 
+                return _store.Collection.Find(Query.EQ("category_id", Id)).ToList<mStores>();
+
+            }
+            catch (MongoConnectionException)
+            {
+                return new List<mStores>();
+            }
+        }
+
+        public mStores Getstores(string name)
+        {
+            var post = _store.Collection.Find(Query.EQ("Name", name)).Single();
+            return post;
+        }
+
+        public mStores Getstore(Guid Id)
+        { 
+            seoWebApplication.Models.mStores query = (from e in _store.Collection.AsQueryable<mStores>()
+                                                      where e.Id == Id
+                                                       select e).First();
+
+            return query;
+        } 
+
+
+        internal void StoreThumbnailUpdate(Guid id, string fileName)
+        {
+            try
+            {
+                var query = Query.And(
+                                        Query<mStores>.EQ(e => e.Id, id)  
+                                    );
+
+                var update = Update<mStores>.Set(e => e.thumbnail, fileName);
+
+                _store.Collection.Update(query, update); 
+            }
+            catch
+            { 
+            } 
+        }
+
+        internal void StoreImageUpdate(Guid id, string fileName)
+        {
+            try
+            {
+                var query = Query.And(
+                                       Query<mStores>.EQ(e => e.Id, id)
+                                   );
+
+                var update = Update<mStores>.Set(e => e.image, fileName);
+
+                _store.Collection.Update(query, update);
+            }
+            catch
+            {
+            }
+        }
+          
+       
+        
+
+           
+
+        internal void Update(mStores p)
+        {  
+            var query = Query<mStores>.EQ(e => e.Id, p.Id); 
+            var update = Update<mStores>.Set(e => e.name, p.name) 
+                                           .Set(e => e.IsActive, p.IsActive)
+                                           .Set(e => e.image, p.image)
+                                           .Set(e => e.thumbnail, p.thumbnail)
+                                           .Set(e => e.description, p.description)
+                                           .Set(e => e.AffiliateId, p.AffiliateId)
+                                           .Set(e => e.Url, p.Url);
+
+            _store.Collection.Update(query, update); 
+        }
+
+        internal void Delete(Guid id)
+        {
+            try
+            {
+                var query = Query<mStores>.EQ(e => e.Id, id);  
+
+                _store.Collection.Remove(query);
+
+            }
+            catch
+            {
+
+            } 
+        }
+
+            
+    }
+}
