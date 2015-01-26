7d9ad01cc0718e11a749e0167b3c2090d039401d
Ty Moodley <ty.moodley@gmail.com>
2015-01-25 23:05:00 -0500

    fixed order by likes.

--- seoWebApplication/Controllers/BaseController.cs
+++ seoWebApplication/Controllers/BaseController.cs
@@ -97,10 +97,10 @@ namespace seoWebApplication.Controllers
                 return null;
 
             IList<mProducts> products = _productService.GetProductsOnFrontPromo();
-
+            var orderList = products.OrderByDescending(x => x.LikesCount);
             // page the list
             const int pageSize = 18;
-            var listPaged = products.ToPagedList(page ?? 1, pageSize);
+            var listPaged = orderList.ToPagedList(page ?? 1, pageSize);
 
             // return a 404 if user browses to pages beyond last page. special case first page if no items exist
             if (listPaged.PageNumber != 1 && page.HasValue && page > listPaged.PageCount)
